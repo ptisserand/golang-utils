cbcdc38e6af56a8a72af5dbc522955dc6734f6c8
robin <patrice@tisserand.org>
2015-08-23 20:15:13 +0200

    finddup: added tool to find duplicate in five list of files

---
+++ finddup/finddup.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	flag.Parse()
+	file, err := os.Open(flag.Args()[0])
+	if err != nil {
+		log.Fatal(err)
+	}	
+	defer file.Close()
+
+	var prefix string
+	if len(flag.Args()) > 1 {
+		prefix = flag.Args()[1]
+		log.Println(prefix)
+	} else {
+		prefix = ""
+	}
+	
+	var index map[string]string
+	index = make(map[string]string)
+	var duplicated map[string][]string
+	duplicated = make(map[string][]string)
+	
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		dir, file := filepath.Split(scanner.Text())
+		if _, ok := index[file]; ok {
+			//log.Println(file, " is already present")
+			if _, ok := duplicated[file]; !ok {
+				duplicated[file] = make([]string, 0)
+				duplicated[file] = append(duplicated[file], filepath.Join(prefix, index[file], file))
+			}
+			duplicated[file] = append(duplicated[file], filepath.Join(prefix, dir, file))
+		} else {
+			index[file] = dir
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for k := range duplicated {
+		sha := make(map[string]string)
+		for _, v := range duplicated[k] {
+			hasher := sha256.New()
+			ff, err := os.Open(v)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer ff.Close()
+			if _, err := io.Copy(hasher, ff); err != nil {
+				log.Fatal(err)
+			}
+			val := hex.EncodeToString(hasher.Sum(nil))
+			if _, ok := sha[val]; !ok {
+				sha[val] = v
+			} else {
+				log.Println("Duplicated sha:", sha[val], " ", v)
+			}
+		}
+	}
+}
+
